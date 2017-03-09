b2e3cca0eb9a1dcc45a988c85cb67740f9c872f6
Janepoor <fjmjp@126.com>
2017-03-09 17:21:42 -0500

    Add another way

--- password generator.py
+++ password generator.py
@@ -4,13 +4,22 @@
 import random
 import string
 
-def gen_pw(size=8, availiable = string.ascii_letters+ string.digits+string.punctuation):
+def gen_pw(size, availiable = string.ascii_letters+ string.digits+string.punctuation):
 
-	return ''.join(random.choice(availiable)) for i in range(size)
+	return ''.join(random.choice(availiable) for i in range(int(size)))
 	
 
 
+passwordlen = raw_input("How long would you like the password to be ? ")
+
+print gen_pw(passwordlen)
 
 
-size= raw_input("How long would you like the password to be ?")
-print gen_pw(size)
+
+# generate a password with length "passlen" with no duplicate characters in the password
+
+
+s = string.ascii_letters+ string.digits+string.punctuation
+passlen = 8
+p =  "".join(random.sample(s,passlen ))
+print p
