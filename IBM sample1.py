00f92d65b1339f9433529c4356ef9ffb2f2e3715
Janepoor <fjmjp@126.com>
2017-03-11 14:18:49 -0500

    Comma issue

--- IBM sample1.py
+++ IBM sample1.py
@@ -1,12 +1,13 @@
 # Given a number N and p,q
 
-
+from __future__ import print_function
 
 
 
 N,p,q,=raw_input("input N and p and q, seperate by a space ").split(' ')
-print N,p,q
 
+
+output=[]
 for i in range(1,int(N)+1):
     indicator=0
     if i%int(p)==0 or i%int(q) ==0:
@@ -14,10 +15,23 @@ for i in range(1,int(N)+1):
     if p in str(i)or q in str(i):
         indicator+=2
     if indicator==3:
-        print"OUTTHINK",
+        output.append("OUTTHINK")
+        #print"OUTTHINK"
     elif indicator==2:
-        print"THINK",
+        output.append("THINK")
+        #print"THINK"
     elif indicator==1:
-        print"OUT",
+        output.append("OUT")
+        #print"OUT"
     else:
-        print i,
+        output.append(i)
+        #print i
+
+    #if i!= int(N):
+        #print",",
+k=0
+for x in output:
+    print (x, end='')
+    k+=1
+    if k != int(N):
+        print (",", end='')
