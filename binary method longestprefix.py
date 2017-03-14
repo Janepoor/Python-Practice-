68fea5604416e475eca144edc2be31fecb919258
Janepoor <fjmjp@126.com>
2017-03-14 02:29:37 -0400

    Add file using binary method

---
+++ binary method longestprefix.py
@@ -0,0 +1,39 @@
+"""public String longestCommonPrefix(String[] strs) {
+    if (strs == null || strs.length == 0)
+        return "";
+    int minLen = Integer.MAX_VALUE;
+    for (String str : strs)
+        minLen = Math.min(minLen, str.length());
+    int low = 1;
+    int high = minLen;
+    while (low <= high) {
+        int middle = (low + high) / 2;
+        if (isCommonPrefix(strs, middle))
+            low = middle + 1;
+        else
+            high = middle - 1;
+    }
+    return strs[0].substring(0, (low + high) / 2);
+}
+
+private boolean isCommonPrefix(String[] strs, int len){
+    String str1 = strs[0].substring(0,len);
+    for (int i = 1; i < strs.length; i++)
+        if (!strs[i].startsWith(str1))
+            return false;
+    return true;
+}"""
+class Solution:
+    # @param strs: A list of strings
+    # @return: The longest common prefix
+    def longestCommonPrefix(self, strs):
+        if len(strs) <= 1:
+            return strs[0] if len(strs) == 1 else ""
+        end, minl = 0, min([len(s) for s in strs])
+        while end < minl:
+            for i in range(1, len(strs)):
+                if strs[i][end] != strs[i-1][end]:
+                    return strs[0][:end]
+            end = end + 1
+
+        return strs[0][:end]
